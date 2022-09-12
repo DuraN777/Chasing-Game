0e6d5fbe1468f3ea0dce103ab969938345246b5c
Djura Neskovic <neskovic991@gmail.com>
2022-09-12 13:17:36 +0200

    up and down demo movement

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
+const chaser = document.querySelector('#chaser');
+const escapee = document.querySelector('#escapee');
 
+
+// https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_key_values#navigation_keys
+window.addEventListener('keydown', (e) => {
+
+  if (e.key === 'ArrowDown' || e.key === 'Down') {
+    const currTop = getPosition(chaser.style.top);
+    chaser.style.top = `${currTop +50}px`;
+  } else if ( e.key === 'ArrowUp' || e.key === 'Up') {
+    const currTop = getPosition(chaser.style.top);
+    chaser.style.top = `${currTop - 50}px`;
+  }
+
+})
+
+
+// Get current position of player (return a number from style)
+// 200px.slice(0, -2) - removes last 2 chars, int this case 'px'
+// Turn string to a number, three ways:
+// 1) Number(pos.slice(0, -2)); 
+// 2) parseInt(pos.slice(0, -2)); 
+// 3) +pos.slice(0, -2);
+function  getPosition(pos) {
+  if (!pos) return 100; // starting pos in css is top: 100px
+  return +pos.slice(0, -2);
+}
 
 
 
